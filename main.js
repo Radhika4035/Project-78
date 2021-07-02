bc89f4561daad13bdfb4f516a1523fa5c423fd90
Radhika4035 <86754890+Radhika4035@users.noreply.github.com>
2021-07-02 10:28:57 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var images = 
+    ["https://st2.depositphotos.com/5425740/11033/v/600/depositphotos_110337550-stock-illustration-family-portrait-at-simple-style.jpg",
+    "https://media.istockphoto.com/vectors/cartoon-business-indian-woman-character-with-present-pose-vector-id918988774?k=6&m=918988774&s=612x612&w=0&h=sTJpTDVxdGN7eQMoP8TUuhxRjZlLMoBQsCltMoDWnc8=",
+    "https://thumbs.dreamstime.com/b/indian-asian-oriental-culture-cartoon-indian-asian-religion-oriental-culture-traditional-family-parents-mother-father-156979274.jpg",
+    "https://media.istockphoto.com/vectors/vector-illustration-indian-grandma-vector-id587207272",
+    "https://image.shutterstock.com/image-vector/vector-graphic-illustration-grandfather-holding-260nw-1788663509.jpg",
+    "https://images.clipartlogo.com/files/istock/previews/1011/101130421-vector-illustration-indian-boy.jpg",
+    "https://media1.popsugar-assets.com/files/thumbor/dYHaCs2C-L7YTU8JT9zkOia0ADQ/701x0:2581x1880/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/14/698/n/24155406/1b234bb15e46c0b32c12b3.12462503_/i/disney-junior-mira-royal-detective-series-details.jpg",];
+    "https://media.istockphoto.com/vectors/maharashtrian-family-showing-culture-of-maharashtra-india-vector-id542728198?k=6&m=542728198&s=612x612&w=0&h=tHt-LleVzPNNf1zUrHOTZXT6aZQUHhPMlacwaCZQ6ek="
+
+var names = 
+["Family Book","Deepthi Subramanya", "Raghunandan Dudda Subramanya","Usha Subramanya","Dudda Krishnamoorthy Subramanya","Darshan Dudda","Radika Dudda"];
